997279616d7aa2dcc159ddefd9570f27face9cf8
eshoiza <engrjayt200@gmail.com>
2020-01-28 11:04:10 +0100

    project setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navbar = (props) => {
+    return (
+      <div>
+        <div className="btnMenu">
+          <label htmlFor="chkMenu">
+            <i className="fa fa-bars"></i>
+          </label>
+        </div>
+        <input type="checkbox" id="chkMenu" />
+        <nav className="menu">
+            <div className="title"
+        </nav>
+      </div>
+    );
+}
+
+export default Navbar;
